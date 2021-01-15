2e421a1d7c16f7ef60275936ed55bdceb51eed03
Valentio <loryyk95@hotmail.com>
2021-01-15 12:06:36 +0100

    Incorporació dels esquelets dels testos.

---
+++ test/medicalconsultation/ConsultationTerminalTest.java
@@ -0,0 +1,45 @@
+package medicalconsultation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultationTerminalTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void initRevision() {
+    }
+
+    @Test
+    void initPrescriptionEdition() {
+    }
+
+    @Test
+    void searchForProducts() {
+    }
+
+    @Test
+    void selectProduct() {
+    }
+
+    @Test
+    void enterMedicineGuidelines() {
+    }
+
+    @Test
+    void enterTreatmentEndingDate() {
+    }
+
+    @Test
+    void sendePrescription() {
+    }
+
+    @Test
+    void printePresc() {
+    }
+}
---
+++ test/medicalconsultation/MedicalPrescriptionTest.java
@@ -0,0 +1,25 @@
+package medicalconsultation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicalPrescriptionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addLine() {
+    }
+
+    @Test
+    void modifyLine() {
+    }
+
+    @Test
+    void removeLine() {
+    }
+}
