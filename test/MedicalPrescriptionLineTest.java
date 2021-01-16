9885fa96d340d738edbae9ee79d1665e5c2e1c4e
marchidalgo <marchidalgo@icloud.com>
2021-01-17 00:15:13 +0100

    Remove dummy import

--- test/MedicalPrescriptionLineTest.java
+++ test/MedicalPrescriptionLineTest.java
@@ -5,7 +5,6 @@ import medicalconsultation.FqUnit;
 import medicalconsultation.MedicalPrescriptionLine;
 import medicalconsultation.TakingGuideline;
 import medicalconsultation.dayMoment;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
