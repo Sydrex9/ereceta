e338455cafbb934edd8e6d9abff4e9860eb0a55e
marchidalgo <marchidalgo@icloud.com>
2021-01-16 19:26:45 +0100

    Add DataHealthCardIDTest

---
+++ test/DataDigitalSignatureTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DataDigitalSignatureTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void initRevision() {
+
+    }
+
+}
+
+
+
---
+++ test/DataProductIDTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataProductIDTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void initRevision() {
+
+    }
+
+}
+
