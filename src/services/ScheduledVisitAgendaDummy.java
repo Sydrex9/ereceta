f1c1e702f490c728506026f24611569386c2633f
spele <spele95@hotmail.com>
2021-01-17 22:41:11 +0100

    HealthNationalServiceDummy implementation.

--- src/services/ScheduledVisitAgendaDummy.java
+++ src/services/ScheduledVisitAgendaDummy.java
@@ -2,6 +2,8 @@ package services;
 
 import data.HealthCardID;
 import exceptions.HealthCardException;
+
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -10,7 +12,9 @@ public class ScheduledVisitAgendaDummy implements ScheduledVisitAgenda{
     private final Date today = new Date();
 
     public ScheduledVisitAgendaDummy() throws Exception{
-        // falta la inicialització de les diferents classes a utilitzar en els testos!!!!
+        sva.put(new Date(99, 11, 27), new HealthCardID("314679172839"));
+        sva.put(new Date(103, 7, 2), new HealthCardID("2154876963652"));
+        sva.put(new Date(120, 4, 24), new HealthCardID("876954321535"));
     }
 
     @Override
@@ -18,5 +22,4 @@ public class ScheduledVisitAgendaDummy implements ScheduledVisitAgenda{
         if (!sva.containsKey(new Date(today.getYear(), today.getMonth(), today.getDay()))) throw new HealthCardException("");
         return sva.get(new Date(today.getYear(), today.getMonth(), today.getDay(), today.getHours(), today.getMinutes())); // we need hours and minutes to specify the visit. If not, we can't assure the unique key value in the hashmap.
     }
-
 }
